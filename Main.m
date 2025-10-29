dba56af93bdca7c9d3da8bcddaa74b027e81f981
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2025-10-29 22:43:45 +0000

    added getting data from dataset functions

--- Main.m
+++ Main.m
@@ -3,3 +3,4 @@ clear; clc; close all
 % Load data
 S = loadData('L8_G3.mat');  % Pass the filename directly as a string
 frame = 1;
+
---
+++ getData.m
@@ -0,0 +1,31 @@
+function [X1, Y1, U1, V1, Z1, X2, Y2, U2, V2, Z2, X3, Y3] = getData(S, frame)
+    % Get Air Data
+    [X1, Y1, U1, V1, Z1] = getAirData(S, frame);
+    
+    % Get Liquid Data
+    [X2, Y2, U2, V2, Z2] = getLiquidData(S, frame);
+    
+    % Get Film Data
+    [X3, Y3] = getFilmData(S, frame);
+end 
+
+function [X1, Y1, U1, V1, Z1] = getAirData(S, frame)
+    X1 = S.all_transposed_x_position_matrix_air(:, :, frame) * 1e3;
+    Y1 = S.all_transposed_y_position_matrix_air(:, :, frame) * 1e3;
+    U1 = S.all_u_matrix_air(:, :, frame);
+    V1 = S.all_v_matrix_air(:, :, frame);
+    Z1 = hypot(U1, V1);
+end
+
+function [X2, Y2, U2, V2, Z2] = getLiquidData(S, frame)
+    X2 = S.all_transposed_x_position_matrix_liquid(:, :, frame) * 1e3;
+    Y2 = S.all_transposed_y_position_matrix_liquid(:, :, frame) * 1e3;
+    U2 = S.all_u_matrix_liquid(:, :, frame);
+    V2 = S.all_v_matrix_liquid(:, :, frame);
+    Z2 = hypot(U2, V2);
+end
+
+function [X3, Y3] = getFilmData(S, frame)
+    X3 = S.film_x_position(1, 15:1090) * 1e3;
+    Y3 = S.smoothed_film_height_matrix_out(15:1090, frame) * 1e3;
+end
