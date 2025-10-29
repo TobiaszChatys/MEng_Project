5bf5647f71c0ce42cdaa6d320ffe48165525a5dd
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2025-10-29 22:59:57 +0000

    updated loadData.m

--- loadData.m
+++ loadData.m
@@ -1,7 +1,5 @@
-function S = loadData(filename)
-    if nargin < 1
-        filename = 'L8_G3.mat';
-    end
+function S = loadData(filename)    
+    filename = 'L8_G3.mat';
     tmp = load(filename);
     S = tmp.S2P_PIV_full_mat_vars;
 end
