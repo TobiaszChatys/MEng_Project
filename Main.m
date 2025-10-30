e06f415c54e5c5c56b14580377a466ef30ca5109
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2025-10-30 09:32:52 +0000

    simplified animation generation and created it into a function to be ran from Main.m

--- Main.m
+++ Main.m
@@ -4,11 +4,14 @@ clear; clc; close all
 S = loadData('L8_G3.mat');  % Pass the filename directly as a string
 frame = 1;
 
-% Get all data at once
+%Get all data at once
 [X1, Y1, U1, V1, Z1, X2, Y2, U2, V2, Z2, X3, Y3] = getData(S, frame);
 
-% Create masks
+%% Create masks
 [Z1_masked, Z2_masked] = createMasks(X1, Y1, X2, Y2, Z1, Z2, X3, Y3);
 
-% Plot results
+%% Plot Velocity Vectors with heatmap
 plotVectorMap(X1, Y1, U1, V1, X2, Y2, U2, V2, X3, Y3, Z1_masked, Z2_masked);
+
+%% Animate Velocity Vectors
+animateVelocityVectors(S);
