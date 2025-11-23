6e88f0a48a82c25e6068b729903c06d8e33f5425
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2025-11-23 18:55:34 +0000

    added v for both liquid and gas and computed the resultant velocity for the given index

--- SimpleAveraged.m
+++ SimpleAveraged.m
@@ -11,9 +11,18 @@ column_index = 25;
 % limit data to valid index:
 U1_air_line = squeeze(S.all_u_matrix_air(:, column_index, 1:frames));
 U2_liquid_line = squeeze(S.all_u_matrix_liquid(:, column_index, 1:frames));
+V1_air_line = squeeze(S.all_v_matrix_air(:, column_index, 1:frames));
+V2_liquid_line = squeeze(S.all_v_matrix_liquid(:, column_index, 1:frames));
+
+
 
 u1_air_mean = mean(U1_air_line, 1, 'omitnan');
+v1_air_mean = mean(V1_air_line, 1, 'omitnan');
 u2_liquid_mean = mean(U2_liquid_line, 1, 'omitnan');
+v2_liquid_mean = mean(V2_liquid_line, 1, 'omitnan');
 
+
+mean_air_velocity_magnitude = hypot(u1_air_mean, v1_air_mean);
+mean_liquid_velocity_magnitude = hypot(u2_liquid_mean, v2_liquid_mean);
 
 
