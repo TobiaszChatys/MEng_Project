823d38ae72b0d85042df0b1339d02e71287914e3
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2025-11-23 18:48:14 +0000

    calculated mean for U1 of liquid and air for all froms at a specific index

--- SimpleAveraged.m
+++ SimpleAveraged.m
@@ -8,8 +8,12 @@ frames = size(S.smoothed_film_height_matrix_out, 2) - 1;
 % picking a fixed column to perform simple time-averaging on
 column_index = 25;
 
-% collect data across all frames:
-U1_all = S.all_u_matrix_air(:, column_index, 1:frames);
+% limit data to valid index:
+U1_air_line = squeeze(S.all_u_matrix_air(:, column_index, 1:frames));
+U2_liquid_line = squeeze(S.all_u_matrix_liquid(:, column_index, 1:frames));
+
+u1_air_mean = mean(U1_air_line, 1, 'omitnan');
+u2_liquid_mean = mean(U2_liquid_line, 1, 'omitnan');
 
 
 
