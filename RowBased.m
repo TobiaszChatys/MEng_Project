8f0d50b548dcfe1e5667bb29868489dafa31bddd
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2026-02-10 15:20:44 +0000

    Added plotting of the velocity vector max with the min and max film heights to visualise how many many rows fit inbetween the film height range. useful to validate code to automatically determine this

--- RowBased.m
+++ RowBased.m
@@ -26,4 +26,30 @@ fprintf('Minimum Film Height: %.2f mm\n', min_film_height);
 fprintf('Maximum Film Height: %.2f mm\n', max_film_height);
 fprintf('Mean Film Height: %.2f mm\n', mean_film_height);
 
+%% plotting
+
+frame = 10;
+[X1, Y1, U1, V1, Z1, X2, Y2, U2, V2, Z2, X3, Y3] = getData(S, frame);
+
+quiver(X1 + 11, Y1, U1, V1, 0.8, 'k');
+hold on;
+quiver(X2 + 11, Y2, U2, V2, 0.8, 'b');
+hold on; 
+plot(X3 + 11, Y3, 'g', 'LineWidth', 4)
+hold off;
+yline(min_film_height, 'r--', 'LineWidth', 2);
+yline(max_film_height, 'r--', 'LineWidth', 2);
+
+xlabel('X Position (mm)');
+ylabel('Y Position (mm)');
+title(sprintf('L8-G3 Velocity Vector Map - Frame %d', frame));
+
+xlim([-15 15]);
+ylim([0 28]);
+
+y_ticks = 0:2:28;
+yticks(y_ticks);
+yticklabels(y_ticks)
+xline(0, 'k--', 'LineWidth', 2);
+
 
