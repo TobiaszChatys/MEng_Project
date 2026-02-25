3b61e6b08a0e8da4dc1b544835823686db741ad0
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2026-02-25 12:46:46 +0000

    updated

--- RowBased.m
+++ RowBased.m
@@ -1,7 +1,7 @@
 %% import data
 clc; clear; close all;
 
-[S, filename] = loadData('L8_G12.mat'); 
+[S, filename] = loadData('L8_G10.mat'); 
 frames = size(S.all_u_matrix_liquid, 3);
 
 use_centerline = true; % Set to true to use centerline, false to use fixed column
@@ -79,7 +79,7 @@ fprintf('Total number of rows within the film height range: %d\n', liquid_count)
 
 %% Binning
 
-number_of_vecors_in_bin = 2;
+number_of_vecors_in_bin = 3;
 number_of_bins = floor(liquid_count / number_of_vecors_in_bin);
 fprintf('Number of bins based on %d vectors per bin: %d\n', number_of_vecors_in_bin, number_of_bins);
 bin_edges = linspace(min_film_height, max_film_height, number_of_bins + 1);
@@ -405,7 +405,9 @@ ylabel('Y Position (mm)', 'FontSize', 12, 'Interpreter', 'latex');
 xlabel('$\overline{u}_{(x_0,y)}$ (ms$^{-1}$)', 'FontSize', 12, 'Interpreter', 'latex');
 ylim([0, 28]); 
 yticks(0:2:28);
-xlim([0, 25]);
+
+x_lim = max(conditional_means(bin).U1_mean) * 1.05; % xlimit with a 5% buffer
+xlim([0, x_lim]);
 xticks([0.1 0.5 1 5 10 25]);
 title(sprintf('Mean Velocity Profiles With Bins Containing %d Vectors Each', number_of_vecors_in_bin), 'FontSize', 14);
 lgd = legend('FontSize', 9); 
@@ -472,7 +474,9 @@ ylabel('Y Position (mm)', 'FontSize', 12,'Interpreter', 'latex');
 xlabel('$u''_{(x_0,y),rms}$ (ms$^{-1}$)', 'FontSize', 12, 'Interpreter', 'latex');
 ylim([0, 28]); 
 yticks(0:2:28);
-xlim([0, 15]);
+
+x_lim = max(rms_fluctuations(bin).U1_rms) * 1.05; % xlimit with a 5% buffer
+xlim([0, x_lim]);
 xticks([0.1 0.5 1 5 10 15]);
 title(sprintf('RMS Fluctuations With Bins Containing %d Vectors Each', number_of_vecors_in_bin), 'FontSize', 14);
 lgd = legend('FontSize', 9); 
