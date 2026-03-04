5810aa494923f27a3550d05934c76fb09f98e824
Tobiasz Chatys <TobiaszChatys1@gmail.com>
2026-03-04 21:37:57 +0000

    using the reshape function U2 and V2 are vectorised into one by matrix 4950 by 2499 containing the information for all the frames

--- Incremental_POD.m
+++ Incremental_POD.m
@@ -5,11 +5,7 @@ clc, clear, close all,
 
 %% LOAD DATA
 
-[S, filename] = loadData('L8_G7.mat');
-
-[X1, Y1, U1, V1, Z1, X2, Y2, U2, V2, Z2, X3, Y3] = getData(S, frame);
-% 1's are the air, 2's are the liquid
-
+[S, filename] = loadData('L8_G7.mat'); 
 frames = size(S.all_u_matrix_liquid, 3);
 
 %% Vectorisation
@@ -17,13 +13,23 @@ frames = size(S.all_u_matrix_liquid, 3);
 % So Every frame of the flow field must be flatterned from a 2D grid into a single, 1D column vector
 
 % obtain the rows and columns of the liquid to setup matrices
-[rows_liquid, columns_liquid, ~] = size(U2); 
+[rows_liquid, columns_liquid, ~] = size(S.all_u_matrix_liquid);
 Spatial_points = rows_liquid * columns_liquid;
 
 % initalise matrices to store vectorised velocity components
 U2_Vectorised = zeros(Spatial_points, frames);
 V2_Vectorised = zeros(Spatial_points, frames);
 
+
+% reshape function: reshape(A, sz), reshapes A using size vector sz, to define the resultant
+% the brackets tell matlab to automatically calculate how many rows are needed to fit all the data
+parfor frame = 1:frames
+    U2_Vectorised(:, frame) = reshape(S.all_u_matrix_liquid(:, :, frame), [], 1);
+    V2_Vectorised(:, frame) = reshape(S.all_v_matrix_liquid(:, :, frame), [], 1);
+end
+
+size(V2_Vectorised)
+size(U2_Vectorised)
 
 
 
